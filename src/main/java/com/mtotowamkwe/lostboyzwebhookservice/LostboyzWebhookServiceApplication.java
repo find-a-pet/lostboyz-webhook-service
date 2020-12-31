78b8b50492cfad1226f7c529cb847e6ed07a937b
Michael Mkwelele <memkwelele@gmail.com>
2020-12-30 19:39:51 -0800

    Initialise repository.

---
+++ src/main/java/com/mtotowamkwe/lostboyzwebhookservice/LostboyzWebhookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzwebhookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostboyzWebhookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostboyzWebhookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtotowamkwe/lostboyzwebhookservice/LostboyzWebhookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzwebhookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostboyzWebhookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
