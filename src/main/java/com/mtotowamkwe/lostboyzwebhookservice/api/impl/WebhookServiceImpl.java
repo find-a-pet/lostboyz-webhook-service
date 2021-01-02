8dafd2e8b81ddb29430e6198cf6a399e426514b2
Michael Mkwelele <memkwelele@gmail.com>
2021-01-01 22:40:57 -0800

    Remove TODO.

--- src/main/java/com/mtotowamkwe/lostboyzwebhookservice/api/impl/WebhookServiceImpl.java
+++ src/main/java/com/mtotowamkwe/lostboyzwebhookservice/api/impl/WebhookServiceImpl.java
@@ -27,7 +27,6 @@ public class WebhookServiceImpl implements WebhookService {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            // TODO: Update URLs to match production endpoints prior to deployment
             response = template.postForEntity(PET_SERVICE_URL, new HttpEntity<>(submission, headers),
                     RedditSubmission.class);
         } catch (Exception e) {
